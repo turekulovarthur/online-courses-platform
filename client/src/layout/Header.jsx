6b03d6993ecaf3d308b2f6dca0692ea1f809ba6d
turekulovarthur <turekulovarthur0@gmail.com>
2025-08-21 14:16:33 +0500

    Добавлены компоненты Header и NavLinks

---
+++ client/src/layout/Header.jsx
@@ -0,0 +1,12 @@
+import 'Header.css'
+import { NavLinks } from './NavLinks'
+
+export const Header = () => {
+    return (
+        <header>
+            <nav>
+                <NavLinks />
+            </nav>
+        </header>
+    )
+}
---
+++ client/src/layout/NavLinks.jsx
@@ -0,0 +1,10 @@
+export const NavLinks = () => {
+    return (
+        <ul>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+            <li><a href="#"></a></li>
+        </ul>
+    )
+}
