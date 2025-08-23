c2dcd1ee33c24886e6b568809340f78829734834
turekulovarthur <turekulovarthur0@gmail.com>
2025-08-23 15:32:02 +0500

    Добавлены ссылки на страницы

--- client/src/layout/NavLinks.jsx
+++ client/src/layout/NavLinks.jsx
@@ -1,10 +1,11 @@
+import { Link } from "react-router-dom"
+
 export const NavLinks = () => {
     return (
         <ul>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
-            <li><a href="#"></a></li>
+            <li><Link to={'/'}>Домащняя страница</Link></li>
+            <li><Link to={'/courses'}>Избранное</Link></li>
+            <li><Link to={'/favorites'}>Курсы</Link></li>
         </ul>
     )
 }
