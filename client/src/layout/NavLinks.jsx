59d93ee79efb3409e5c1725d4af3b12ace149e1d
turekulovarthur <turekulovarthur0@gmail.com>
2025-08-27 17:53:57 +0500

    Подключение стилей NavLinks

--- client/src/layout/NavLinks.jsx
+++ client/src/layout/NavLinks.jsx
@@ -1,11 +1,18 @@
 import { Link } from "react-router-dom"
+import styles from './NavLinks.module.css'
 
 export const NavLinks = () => {
     return (
-        <ul>
-            <li><Link to={'/'}>Домащняя страница</Link></li>
-            <li><Link to={'/courses'}>Избранное</Link></li>
-            <li><Link to={'/favorites'}>Курсы</Link></li>
+        <ul className={styles.navList}>
+            <li>
+                <Link to={'/'}>Домашняя страница</Link>
+            </li>
+            <li>
+                <Link to={'/courses'}>Избранное</Link>
+            </li>
+            <li>
+                <Link to={'/favorites'}>Курсы</Link>
+            </li>
         </ul>
     )
 }
