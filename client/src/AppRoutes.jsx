a5e6d330f8cd4f4b9f494a44da07e3394c656754
turekulovarthur <turekulovarthur0@gmail.com>
2025-08-27 17:20:22 +0500

    Исправлены маршруты

--- client/src/AppRoutes.jsx
+++ client/src/AppRoutes.jsx
@@ -1,5 +1,7 @@
 import { Routes, Route } from 'react-router-dom'
 
+import Layout from './layout/Layout'
+
 import { Home } from './pages/Home'
 import { Favorites } from './pages/Favorites'
 import { Courses } from './pages/Courses'
@@ -7,9 +9,11 @@ import { Courses } from './pages/Courses'
 export const AppRoutes = () => {
     return (
         <Routes>
-            <Route path='/' element={<Home />} />
-            <Route path='/favorites' element={<Favorites />} />
-            <Route path='/courses' element={<Courses />} />
+            <Route path='/' element={ <Layout /> }>
+                <Route index element={<Home />} />
+                <Route path='/favorites' element={<Favorites />} />
+                <Route path='/courses' element={<Courses />} />
+            </Route>
         </Routes>
     )
 }
