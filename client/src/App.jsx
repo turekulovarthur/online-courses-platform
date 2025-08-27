023dec02d6610c1aea59d14cf1777461e9344e8d
turekulovarthur <turekulovarthur0@gmail.com>
2025-08-27 17:18:36 +0500

    Исправлена ошибка с Route внутри BrowserRouter

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,13 +1,10 @@
-import { BrowserRouter, Route } from "react-router-dom"
+import { BrowserRouter } from "react-router-dom"
 import { AppRoutes } from "./AppRoutes"
-import Layout from "./layout/Layout"
 
 export default function App() {
   return (
-    <BrowserRouter>
-        <Route element={ <Layout /> }> 
-            <AppRoutes/>
-        </Route>
+    <BrowserRouter> 
+      <AppRoutes/> 
     </BrowserRouter>
   )
 }
